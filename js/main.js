40d7f57be79460c429aa88ed91e9ce5c4fea1c76
bobby5develops <bobby5develops@gmail.com>
2015-04-12 01:16:11 -0400

    first problem solved with simple jquery vs angularjs solution

---
+++ js/main.js
@@ -0,0 +1,9 @@
+ /*solving problem 1: How to watch a user input and update div simultaneously*/
+ /*$(function(){
+    var name = $('#name');
+    var greeting = $('#greeting');
+    /*define event listener for mouse event 'keyup' to capture value of name*/
+    /*name.keyup(function(){
+    	greeting.text(name.val());
+    });
+ }); */
